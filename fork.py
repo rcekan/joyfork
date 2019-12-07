7268920170faa7c77fdd596da6c85605164aa638
rcekan <cekander@gmail.com>
2019-12-07 16:53:58 -0600

    added secret key and client id for oauth

--- fork.py
+++ fork.py
@@ -4,8 +4,8 @@ from flask_dance.contrib.github import make_github_blueprint, github
 app = Flask(__name__)
 app.secret_key = 'expialadocious'
 blueprint = make_github_blueprint(
-    client_id='',
-    client_secret='',
+    client_id='a79f2f917eaff7383909',
+    client_secret='2228fffc207476054bb8cd7c54eefffc8ce0b989',
 )
 app.register_blueprint(blueprint, url_prefix='/login')
 
